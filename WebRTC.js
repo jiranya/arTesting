4b86492ecbbf6f663d2e467dd1605c49cfe1e5ed
jiranya <38207920+jiranya@users.noreply.github.com>
2022-01-14 14:03:41 +0700

    Create WebRTC.js

---
+++ WebRTC.js
@@ -0,0 +1,6 @@
+(function() {
+  if (!window.navigator) window.navigator = {};
+  window.navigator.getUserMedia = function() {
+    webkit.messageHandlers.callbackHandler.postMessage(arguments);
+  }
+})();
